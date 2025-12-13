d8c26bb0805e5b1b48832edcc30667bee7d79747
cihanumut <cihanumut_colak@hotmail.com>
2025-12-13 14:35:34 +0300

    fix

--- app_api/controller/VenueController.js
+++ app_api/controller/VenueController.js
@@ -54,32 +54,32 @@ const listVenues = function (req, res) {
 };
 
 const addVenue = async function (req, res) {
-    try{
-    await Venue.create({
-    ...req.body,
-    coordinates:[req.body.lat,req.body.long],
-    hours:[{
-        day: req.body.days1,
-        open: req.body.open1,
-        close: req.body.close1,
-        isClosed: req.body.isClosed1,
-    },
-        {
-        day: req.body.days2,
-        open: req.body.open2,
-        close: req.body.close2,
-        isClosed: req.body.isClosed2,
-        },
-    ],
+    try{ await Venue.create({
+        ...req.body,
+        coordinates:[req.body.lat,req.body.long],
+        hours:[
+            {
+                day:req.body.days1,
+                open:req.body.open1,
+                close:req.body.close1,
+                isClosed:req.body.isClosed1,
+
+            },
+            {
+                 day:req.body.days2,
+                open:req.body.open2,
+                close:req.body.close2,
+                isClosed:req.body.isClosed2,
+            },
+        ],
     }).then(function(response){
         createResponse(res,"201",response);
+
     });
-}
-    catch(error){
-        createResponse(res,"400",error);
+    }catch(error){
+    createResponse(res, "400", error);
     }
-//createResponse(res, 200, { status: "başarılı" });
-};
+    };
 
 const getVenue = async function (req, res) {
     try {
@@ -94,42 +94,51 @@ const getVenue = async function (req, res) {
     //createResponse(res,200,{status:"getvenue başarılı"});
 }
 
-const updateVenue = async function (req, res) {
-    try {
-        const updatedVenue = await Venue.findByIdAndUpdate(req.params.venueid,
+const updateVenue =async function (req, res) {
+try{
+    const updateVenue=await Venue.findByIdAndUpdate(
+        req.params.venueid,
         {
             ...req.body,
             coordinates:[req.body.lat,req.body.long],
-    hours:[{
-        day: req.body.days1,
-        open: req.body.open1,
-        close: req.body.close1,
-        isClosed: req.body.isClosed1,
+            hours:[
+                {
+                    day:req.body.days1,
+                    open:req.body.open1,
+                    close:req.body.close1,
+                    isClosed:req.body.isClosed1
+                },
+                {
+                    day:req.body.days2,
+                    open:req.body.open2,
+                    close:req.body.close2,
+                    isClosed:req.body.isClosed2
+                }
+            ]
         },
-        {
-        day: req.body.days2,
-        open: req.body.open2,
-        close: req.body.close2,
-        isClosed: req.body.isClosed2,
-        },
-    ]
-        },   
-    {new:true}
+        {new:true}
     );
-createResponse(res,201,updatedVenue);
-    }catch(error){
-        createResponse(res,400,{status:"Güncelleme başarısız",error});
-    };
+    createResponse(res,201,updatedVenue);
+}catch(error){
+    createResponse(res,400,{ststus:"güncelleme başarısız",error});
+    
 }
-const deleteVenue = async function (req, res) {
-    try{
-        await Venue.findByIdAndDelete(req.params.venueid).then(function(venue){
-            createResponse(res,200,{status:venue.name+"isimli mekan silindi"});
-        });
-    }catch(error){
-        createResponse(res,404,{status:"Böyle bir mekan yok!"});
-    }
+
+
 };
+
+const deleteVenue = async function (req, res) {
+try{
+    await Venue.findByIdAndDelete(req.params.venueid).then(function(venue)
+{
+    createResponse(res,200,{status:venue.name+"isimli mekan silindi"});
+});
+}
+catch(error)
+{
+    createResponse(res,404,{status: "böyle bir mekan yok!"});
+
+}};
 
 module.exports = {
     listVenues,
