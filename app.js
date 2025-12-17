b766c84f64b92723371c22f9c00199793d4fed7c
cihanumut <cihanumut_colak@hotmail.com>
2025-12-17 11:30:38 +0300

    fix

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ var usersRouter = require('./routes/users');
 require('./app_api/models/db');
 var apiRoute= require('./app_api/routes/index');
 var app = express();
-
+var allowCrossDomain=function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers','Content-Type,Authorization');
+    next();
+}
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
